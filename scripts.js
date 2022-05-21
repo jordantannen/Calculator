20362402ea8983c7d0b4b857cf0da7260b170030
Jordan Tannenbaum <jordantannen@gmail.com>
2022-05-21 14:33:23 -0400

    Add calculator operational support. Rounded decimals.

--- scripts.js
+++ scripts.js
@@ -18,15 +18,62 @@ function divide(a, b){
 // Core general functions
 function operate(a, b, c){
     if (c == "+")
-        return add(a, b);
+        return roundTwo(add(a, b));
     else if (c == "-")
-        return subtract(a, b);
+        return roundTwo(subtract(a, b));
     else if (c == "*")
-        return multiply(a, b);
+        return roundTwo(multiply(a, b));
     else if (c == "/")
-        return divide(a, b);
-    return "error";
+        return roundTwo(divide(a, b));
 }
 
-// Live code
-let displayVal = parseFloat(document.getElementById("display").innerHTML);
+function roundTwo(num){
+    return Math.round(num * 100) / 100;
+}
+
+// Display and value holder variables
+let liveDisplay = document.getElementById("display");
+let currentVal = null;
+let lastOpp = null;
+let newNum = true;
+
+// Buttons
+let numButtons = document.getElementsByClassName("num-button");
+let oppButtons = document.getElementsByClassName("opp-button");
+
+// Clear button
+document.getElementById("clear").addEventListener("click", () => {
+    currentVal = null;
+    lastOpp = null;
+    liveDisplay.innerHTML = "";
+});
+
+// Delete button
+document.getElementById("delete").addEventListener("click", () => {
+    liveDisplay.innerHTML = Array.from(liveDisplay.innerHTML).splice(-1,1);
+});
+
+// Get num button input
+for (let i = 0; i < numButtons.length; i++){
+    numButtons[i].addEventListener("click", () => {
+        if (newNum == true) {
+            liveDisplay.innerHTML = numButtons[i].innerHTML;
+            newNum = false;
+        }
+        else
+            liveDisplay.innerHTML += numButtons[i].innerHTML;
+    });
+}
+
+// Get opp button input
+for (let i = 0; i < oppButtons.length; i++){
+    oppButtons[i].addEventListener("click", () => {
+        if (currentVal != null && lastOpp != "=") {
+            liveDisplay.innerHTML = operate(currentVal, parseFloat(liveDisplay.innerHTML), lastOpp);
+        }
+        currentVal = parseFloat(liveDisplay.innerHTML);
+        lastOpp = oppButtons[i].innerHTML;
+        newNum = true;
+    });
+}
+
